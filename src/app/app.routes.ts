b13b38f76357e626308a4e0be307de6d41427987
Ikeda Harket <sebaaignacio111@gmail.com>
2026-04-23 16:07:31 -0400

    anami-ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () => import('@layouts/auth-layout/auth-layout').then((c) => c.AuthLayout),
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('@pages/auth/login-page/login-page').then((c) => c.LoginPage),
+      },
+    ],
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('@layouts/dashboard-layout/dashboard-layout').then((c) => c.DashboardLayout),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@pages/dashboard/home-page/home-page').then((c) => c.HomePage),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import '@ikedadev/anami-ui/dist/anami.css';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: '<router-outlet />',
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('anami-dashboard');
