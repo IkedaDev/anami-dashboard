38f08ce4e16847a37835aaf9988ca4d645a43f9e
Ikeda Harket <sebaaignacio111@gmail.com>
2026-05-26 17:19:47 -0400

    services fix

--- src/app/models/business/service.model.ts
+++ src/app/models/business/service.model.ts
@@ -7,4 +7,5 @@ export interface Service {
   isActive: boolean;
   createdAt: Date;
   updatedAt: Date;
+  available: boolean;
 }
--- src/app/pages/dashboard/services-page/service-form/service-form.ts
+++ src/app/pages/dashboard/services-page/service-form/service-form.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Service } from '@models/business';
 import { DrawerService, ServicePageService } from '@services/index';
 import { useFormUtils } from '@utils/form.utils';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-service-form',
@@ -14,12 +15,14 @@ export class ServiceForm implements OnInit {
   public service = inject(ServicePageService);
   public drawer = inject(DrawerService);
   public data = input();
+  public isSaving = signal(false);
+
   public serviceForm = inject(FormBuilder).group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     description: ['', [Validators.maxLength(200)]],
     price: [0, [Validators.required, Validators.min(0)]],
     duration: [0, [Validators.required, Validators.min(0)]],
-    isActive: [true],
+    available: [true],
   });
 
   public formUtils = useFormUtils(this.serviceForm);
@@ -31,14 +34,20 @@ export class ServiceForm implements OnInit {
   }
 
   saveService() {
-    // if (this.serviceForm.invalid) {
-    //   this.serviceForm.markAllAsTouched();
-    //   return;
-    // }
-    // const formData = this.serviceForm.getRawValue() as Service;
-    // this.service.selectedService.set(formData);
-    // this.service.save();
-    // this.isDrawerOpen.set(false);
-    // console.log('Servicio procesado con éxito:', formData.name);
+    if (this.serviceForm.invalid) {
+      this.serviceForm.markAllAsTouched();
+      return;
+    }
+    const formData = this.serviceForm.getRawValue() as any as Service;
+
+    const request$ = this.service.selectedService()
+      ? this.service.update(this.service.selectedService()!.id, formData)
+      : this.service.create(formData);
+
+    this.isSaving.set(true);
+
+    request$.pipe(finalize(() => this.isSaving.set(false))).subscribe({
+      next: () => this.drawer.close(),
+    });
   }
 }
